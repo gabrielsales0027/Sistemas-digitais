1f169b417973bf69ed125e66e84d174fe0745177
gabrielsales0027 <45940503+gabrielsales0027@users.noreply.github.com>
2019-03-26 09:33:28 -0300

    Gerador de paridade v1

---
+++ Aulas/26.03.2019/geradorparidade.vhd
@@ -0,0 +1,15 @@
+entity geradorparidade is
+generic (n: in integer := 32);
+port(input: in bit_vector(n-1 downto 0);
+	  output: out bit);
+end geradorparidade;
+
+architecture gerador of geradorparidade is
+signal s : bit_vector(n downto 0);
+begin
+laco : for i  in 0 to n-1 generate
+		s(i+1) <= s(i) xor input(i);
+end generate;
+s(0) <= '0';
+output <= s(n);
+end gerador;
