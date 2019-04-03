659659f6e6cb030379eb70e2238fb900a052f2e7
gabrielsales0027 <45940503+gabrielsales0027@users.noreply.github.com>
2019-04-03 08:19:20 -0300

    Update somador.vhd

--- Aulas/03.04.2019/somador.vhd
+++ Aulas/03.04.2019/somador.vhd
@@ -1,1 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity somador is
+generic(n: integer := 8);
+port(a,b: in std_logic_vector(n-1 downto 0);
+		s: out std_logic_vector(n-1 downto 0));
+end somador;
+
+architecture arq of somador is
+begin
+
+s <= std_logic_vector(signed(a)+signed(b));
+
+end arq;
