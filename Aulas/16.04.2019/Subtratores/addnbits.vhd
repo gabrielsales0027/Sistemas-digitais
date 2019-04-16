91357b380df373656bbf43492fbb6b020113f74d
gabrielsales0027 <45940503+gabrielsales0027@users.noreply.github.com>
2019-04-16 09:59:57 -0300

    Create addnbits.vhd
    Pra fazer o subtrator precisa do somador

---
+++ Aulas/16.04.2019/Subtratores/addnbits.vhd
@@ -0,0 +1,30 @@
+-- Somador de inteiros
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity addnbits is
+generic(n : integer := 4);
+port(a,b : in std_logic_vector(n-1 downto 0);
+		s : out std_logic_vector(n-1 downto 0);
+		cin : in std_logic;
+		cout : out std_logic);
+end addnbits;
+architecture arq of addnbits is
+
+component add1b is
+port(a,b,cin : in std_logic;
+		s,cout: out std_logic);
+end component;
+
+signal carry : std_logic_vector(n downto 0);
+
+begin
+
+laco: for i in 0 to n-1 generate
+adds: add1b port map(a(i),b(i),carry(i),s(i),carry(i+1));
+end generate;
+
+carry(0) <= cin;
+cout <= carry(n);
+
+end arq;
